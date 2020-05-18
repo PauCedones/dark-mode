abf040bf8865b21ed0f273a0c9ffa9cb5807ab17
Paula <cedones@gmail.com>
2020-05-17 22:09:41 -0300

    clases calculadas

--- src/header.js
+++ src/header.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import './header.css'
 
+import Switch from './switch.js';
+
 
 function Header() {
     return(
@@ -11,11 +13,7 @@ function Header() {
             <h1>Social Media Dashboard</h1>
             <p className="header-total">Total followers: 23, 004</p>
           </div>
-          <div className="dark-mode">
-            <p className="dark-mode-title">Dark Mode</p>
-            <input type="checkbox" className="checkbox" id="checkbox"/>
-            <label className="switch" htmlFor="checkbox"></label>
-          </div>
+          <Switch />
         </div>
       </div>
     </header>
--- src/top-card-list.js
+++ src/top-card-list.js
@@ -1,25 +1,52 @@
 import React from 'react';
+import Card from './card.js';
+
+const cardListData = [
+  {
+    username:'@Pepito',
+    id: 1,
+    followers: '1500',
+    todayFollowers: 12,
+    icon:'images/icon-facebook.svg',
+    name: 'facebook',
+  },
+  {
+    username:'@Pepitus',
+    id: 2,
+    followers: '5k',
+    todayFollowers: 21,
+    icon:'images/icon-twitter.svg',
+    name: 'twitter',
+  },
+  {
+    username:'@Papito',
+    id: 3,
+    followers: '28k',
+    todayFollowers: 30,
+    icon: 'images/icon-instagram.svg',
+    name: 'instagram',
+  },
+  {
+    username:'@Patata',
+    id: 4,
+    followers: '6k',
+    todayFollowers: -50,
+    icon: 'images/icon-youtube.svg',
+    name: 'youtube',
+  },
+]
 
 function TopCardList () {
     return(
         <section className="top-cards">
     <div className="wrapper">
       <div className="grid">
-        <article className="card facebook">
-          <p className="card-title">
-            <img src="images/icon-facebook.svg" alt=""/>
-            @LeonidasEsteban
-          </p>
-          <p className="card-followers">
-            <span className="card-followers-number">1483</span>
-            <span className="card-followers-title">Followers</span>
-          </p>
-          <p className="card-today">
-            <img src="images/icon-up.svg" alt=""/>
-            12 Today
-          </p>
-        </article>
-        <article className="card twitter">
+        {
+          cardListData.map( (cardData) => <Card key={cardData.id} {...cardData} /> )
+        }
+        
+        
+       {/*  <article className="card twitter">
           <p className="card-title">
             <img src="images/icon-twitter.svg" alt=""/>
             @LeonidasEsteban
@@ -60,7 +87,7 @@ function TopCardList () {
             <img src="images/icon-up.svg" alt=""/>
             12 Today
           </p>
-        </article>
+        </article> */}
       </div>
     </div>
   </section>
