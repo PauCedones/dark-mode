ab4ec29b3fadad609912d7edfe01f4b8f52685fa
Paula <cedones@gmail.com>
2021-03-11 15:44:53 -0300

    Updates

---
+++ precache-manifest.643625a7fb2de06e27b85f556e3f32f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a6eaa21194cf5893317bb1b1db801ae",
+    "url": "/dark-mode/index.html"
+  },
+  {
+    "revision": "baa035a8519d18eefd53",
+    "url": "/dark-mode/static/css/main.00afb407.chunk.css"
+  },
+  {
+    "revision": "f44e8fb3bb21bf853cce",
+    "url": "/dark-mode/static/js/2.fb4f5cc7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dark-mode/static/js/2.fb4f5cc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "baa035a8519d18eefd53",
+    "url": "/dark-mode/static/js/main.bae92877.chunk.js"
+  },
+  {
+    "revision": "344422e4522963cde8e0",
+    "url": "/dark-mode/static/js/runtime-main.ef9503e6.js"
+  }
+]);
